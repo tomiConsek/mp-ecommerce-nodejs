ea0f64fab9d4d1aa150f3bf884ebe1c1bc78adfd
tomiConsek <jtomaschiesa@gmail.com>
2020-12-07 20:19:34 -0300

    fix test

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ mercadopago.configure({
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static('assets'));
 
 app.use('/assets', express.static(__dirname + '/assets'));
